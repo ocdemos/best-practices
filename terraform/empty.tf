8f04351f40853df52fec3c0c82ec8bc98c2cca8b
Nicki Watt <nickithewatt@gmail.com>
2017-08-23 09:39:09 +0100

    Added terraform backend config

--- terraform/empty.tf
+++ terraform/empty.tf
@@ -2,3 +2,10 @@
  * Intentionally empty. Required to allow Terraform push
  * commands from the top-level terraform directory.
  */
+
+# Store this config in Terraform Enterprise
+terraform {
+  backend "atlas" {
+    address = "https://atlas.hashicorp.com"
+  }
+}
